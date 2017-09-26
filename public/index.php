e7cb2ddbac2ecced9e5e0779512e04a0965d45fe
Stepan Legachev <siberex@gmail.com>
2017-09-26 19:52:13 +0300

    major outage update. also Sasha is PIZDANOOLSYA

--- public/index.php
+++ public/index.php
@@ -29,64 +29,12 @@ $address    = $addresses[array_rand($addresses)] . '@irkfap.com';
 Contact: <a href="mailto:<?php echo $address; ?>"><?php echo $address; ?></a>
 <br/>
 
-<pre>
- ____________
-&lt; Irkfap.com &gt;
- ------------
-        \   ^__^
-         \  (oo)\_______
-            (__)\       )\/\
-                ||----w |
-                ||     ||
-</pre>
 
-<br/>
+<h1>We are expiriencing a MAJOR OUTAGE right now</h1>
+<h3>Sasha is <i>PIZDANOOLSYA</i></h3>
+<h1>Wait for the updates</h1>
 
-
-When will I be able to buy cheap GPU for my PC from bankrupt miners? Answer is right here!
-
-<!-- TradingView Widget BEGIN -->
-<div style="max-width: 600px; margin: 20px 0;">
-    <div id="tv-medium-widget-a789d"></div>
-</div>
-<script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
-<script type="text/javascript">
-    new TradingView.MediumWidget({
-        "container_id": "tv-medium-widget-a789d",
-        "symbols": [
-            [
-                "ETH",
-                "COINBASE:ETHUSD|3m"
-            ],
-            [
-                "BTC",
-                "COINBASE:BTCUSD|3m"
-            ]
-        ],
-        "gridLineColor": "#e9e9ea",
-        "fontColor": "#83888D",
-        "underLineColor": "#dbeffb",
-        "trendLineColor": "#4bafe9",
-        "width": "600px",
-        "height": "330px",
-        "locale": "en",
-        "chartOnly": true
-    });
-</script>
-<!-- TradingView Widget END -->
-
-<!--
-<div class="btcwdgt-chart" bw-theme="light"></div>
-<script>
-    (function(b,i,t,C,O,I,N) {
-        window.addEventListener('load',function() {
-            if(b.getElementById(C))return;
-            I=b.createElement(i),N=b.getElementsByTagName(i)[0];
-            I.src=t;I.id=C;N.parentNode.insertBefore(I, N);
-        },false)
-    })(document,'script','https://widgets.bitcoin.com/widget.js','btcwdgt');
-</script>
--->
+<h3>Also good news: our website now supports HTTP/2!</h3>
 
 
 <?php date_default_timezone_set('Europe/London'); ?>
