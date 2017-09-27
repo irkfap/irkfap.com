79a67e410be200f22c2d1b1be7b5e2cf95821d2d
Stepan Legachev <siberex@gmail.com>
2017-09-28 01:00:35 +0300

    🐩

--- public/invite.php
+++ public/invite.php
@@ -1,11 +1,11 @@
 <!doctype html>
 <html lang="en">
 <head>
-<?php include_once './_header.php'; ?>
+<?php $t='Irkfap Community Invitation'; include_once './_header.php'; ?>
 </head>
 <body role="document">
 
-    <h1>Irkfap Community</h1>
+    <h1>Irkfap Community 3.0 update</h1>
     <h2><a href="https://t.me/joinchat/AE-ypAzd-Mk4olLR0slxxg">Click on your own risk</a></h2>
     <br>
 
