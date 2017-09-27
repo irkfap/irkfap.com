97532a9231a83f83022c398ca326784d7d146c6f
Stepan Legachev <siberex@gmail.com>
2017-09-27 23:53:40 +0300

    updated about

--- public/about.php
+++ public/about.php
@@ -22,36 +22,37 @@
 
 <h3>Our team:</h3>
 
-Alexander Batsuev — Deposed Rebel Team Lead<br/>
-Sergeii Korobov — Lead Fortress Remote Developer<br/>
-Maxim Perelygin — Lead Steampowered Publisher<br/>
-Stepan Legachev — Lead Nose Picker<br/>
-Sergay Kiselev — Lead Whining Crybaby Engineer<br/>
-Vyacheslav Kovalyov — Lead Wannabe Mini Cooper Driver<br/>
-Dmitrii Sizikov — Lead Bicycle Driver and Community Renovator<br/>
-Yulia Dubinina — Lead Wannabe Senior Java Developer<br/>
-Ilya Chernikov — Lead Connectivity Provider<br/>
-Anton Chaporgin — Lead Search Engine Insider<br/>
-Alexander Kuzmin — Lead Fiction Writer<br/>
-Eugene Borman Elesin — Offshore Opposition Support Team Lead<br/>
-Oleg Roschupkin — Lead Emergency Dick Pic Provider<br/>
-Vladimir Goshev — Lead Cat Horde Handler<br/>
-Anton Baranov — Lead Dog Horde Handler<br/>
-Ivan Sidorov — Lead Nietzsche Interpreter &amp; Drug Expert<br/>
-Vladimir Smirnov — Lead Instagram Content Provider<br/>
-Ilya Kobelkov — Lead Vehicle Condition Assessor<br/>
-Ivan Voronkov — Lead WTF and Wat Content Generator<br/>
-Maxim Penzin — Lead Weather Forecast Provider<br/>
-Dmitrii Ionych Stepanov — Lead Senior Delphi Developer<br/>
-Kirill Fatin — Lead Craft &amp; Draft Beers Expert<br/>
-Dmitrii Chumak — Lead Crowdsourced Translation Provider<br/>
-Dmitriii Lozhkin — Lead Horse Horde Handler<br/>
-Sergeii Zynx Petrov — Lead Fertility Provider<br/>
-Anton Chernousov — Lead Meetup Organiser<br/>
-Eugene Potapov — Lead DevOps Horde Handler<br/>
-Anton Malyshev — Lead Irish Ambassador<br/>
-Vitaly Vitroot Ivaschenko — Lead Ukraine-outsourced SysAdmin<br/>
-<br/>
+Alexander Batsuev — Deposed Rebel Team Lead<br>
+Sergeii Korobov — Lead Fortress Remote Developer<br>
+Maxim Perelygin — Lead Steampowered Publisher<br>
+Stepan Legachev — Lead Nose Picker<br>
+Sergay Kiselev — Lead Whining Crybaby Engineer<br>
+Vyacheslav Kovalyov — Lead Wannabe Mini Cooper Driver<br>
+Dmitrii Sizikov — Lead Bicycle Driver and Community Renovator<br>
+Yulia Dubinina — Lead Wannabe Senior Java Developer<br>
+Ilya Chernikov — Lead Connectivity Provider<br>
+Anton Chaporgin — Lead Search Engine Insider<br>
+Alexander Kuzmin — Lead Fiction Writer<br>
+Eugene Borman Elesin — Offshore Opposition Support Team Lead<br>
+Oleg Roschupkin — Lead Emergency Dick Pic Provider<br>
+Vladimir Goshev — Lead Cat Horde Handler<br>
+Anton Baranov — Lead Dog Horde Handler<br>
+Ivan Sidorov — Lead Nietzsche Interpreter &amp; Drug Expert<br>
+Vladimir Smirnov — Lead Instagram Content Provider<br>
+Ilya Kobelkov — Lead Vehicle Condition Assessor<br>
+Ivan Voronkov — Lead WTF and Wat Content Generator<br>
+Maxim Penzin — Lead Weather Forecast Provider<br>
+Dmitrii Ionych Stepanov — Lead Senior Delphi Developer<br>
+Kirill Fatin — Lead Craft &amp; Draft Beers Expert<br>
+Dmitrii Chumak — Lead Crowdsourced Translation Provider<br>
+Dmitriii Lozhkin — Lead Horse Horde Handler<br>
+Sergeii Zynx Petrov — Lead Fertility Provider<br>
+Anton Chernousov — Lead Meetup Organiser<br>
+Eugene Potapov — Lead DevOps Horde Handler<br>
+Anton Malyshev — Lead Irish Ambassador<br>
+Vitaly Vitroot Ivaschenko — Lead Ukraine-outsourced SysAdmin<br>
+Lidia Mashechko — Lead Hand Made Expert<br>
+<br>
 
 
 <?php include_once './footer.php'; ?>
