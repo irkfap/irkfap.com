90c603b87419b6eecf877c4eb1ebb7b869d7f43d
Stepan Legachev <siberex@gmail.com>
2017-11-09 14:37:41 +0800

    updated timeline

--- public/status.php
+++ public/status.php
@@ -18,13 +18,26 @@
 <dl>
 
     <dt><?php date_default_timezone_set('Europe/London'); echo date('d.m.Y'); ?></dt>
-    <dd>All units are operational.</dd>
+    <dd><p>All units are operational.</p></dd>
 
+
+    <dt>08.11.2017</dt>
+    <dd>
+        <p>Successfully finished crowdfunding for <a href="https://vk.com/cdttrob" rel="nofollow">Robotics department</a> of the <a href="http://cdtt38.ru/" rel="nofollow">Irkutsk junior school</a>.</p>
+        <p><img src="./img/cdtt38.ru.jpg" width="640" height="480" alt="Julia Sergeeva and students of cdtt38.ru"></p>
+        <p>Special thanks to:
+            Sergeii Korobov, Anton Malyshev, Anton Chaporgin, Vladimir Goshev,
+            Alexander Scheglov, Konstantin Melnikov, Stepan Legachev, Alexei Konoplev,
+            Alexander Malikov, Maxim Perelygin, Eugene Potapov, Maxim Penzin,
+            Vyacheslav Kovalyov and Vladimir Martyushov.
+        </p>
+    </dd>
 
     <dt>27.09.2017</dt>
     <dd>
         <h1>All hail Irkfap 3.0!</h1>
-        <a href="/invite"><img src="./img/good-news.jpg" width="620" height="624" alt="Good news, everyone! We are back!"></a>
+        <h3>Good news, everyone! <a href="/invite">We are back</a>!</h3>
+        <p><img src="./img/good-news.jpg" width="620" height="624" alt="Good news, everyone! We are back!"></p>
     </dd>
 
 
@@ -35,7 +48,7 @@
 
         <h3><a href="./img/keep-calm.jpg">Keep calm</a> and <s>be a pandicorn</s> wait for the updates</h3>
 
-        <img src="./img/breaking-news.jpg" width="640" height="360" alt="Breaking news: Sasha is pisdanoolsya">
+        <p><img src="./img/breaking-news.jpg" width="640" height="360" alt="Breaking news: Sasha is pisdanoolsya"></p>
 
         <h3>Also the good news: our website now supports HTTP/2!</h3>
     </dd>
@@ -52,7 +65,7 @@
 
     <dt>13.03.2015</dt>
     <dd>
-        <h1>Irkfap 2.5 released under TeleFork working title</h1>
+        <h1>Irkfap 2.5 is released under TeleFork working title</h1>
         <h3>The board has taken decision to sell all Skype shares and invest in Telegram growth instead</h3>
     </dd>
 
