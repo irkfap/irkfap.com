f53238cd37e134d933509200f09f38586d76cacb
Stepan Legachev <siberex@gmail.com>
2018-03-29 16:58:01 +0800

    updated invite link

--- public/invite.php
+++ public/invite.php
@@ -3,11 +3,20 @@
 <head>
 <?php $t='Irkfap Community Invitation'; include_once './_header.php'; ?>
 </head>
+<script>
+    var inviteCode = '<?php echo fa16e3ac; ?>';
+</script>
 <body role="document">
 
-    <h1>Irkfap Community 3.0 update</h1>
-    <h2><a href="https://t.me/joinchat/AE-ypAzd-Mk4olLR0slxxg">Click on your own risk</a></h2>
+    <h1>Irkfap Community 3.0</h1>
+    <h2><a id="a-invite" href="https://t.me/durov" rel="nofollow">Click at your own risk</a></h2>
 
+    <script>
+        // Crawler semi-protection
+        var a = document.getElementById('a-invite');
+        var link = a.getAttribute('href');
+        a.setAttribute( 'href', link.replace('durov', 'joinchat/' + inviteCode) );
+    </script>
 <?php include_once './_footer.php'; ?>
 </body>
 </html>
