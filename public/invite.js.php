669b51b55b0a47a97c188472506f7d025df01d64
Stepan Legachev <siberex@gmail.com>
2018-03-30 07:42:11 +0800

    fixed key for production env

--- public/invite.js.php
+++ public/invite.js.php
@@ -6,5 +6,5 @@ header('Cache-Control: no-cache, must-revalidate');
 
 $uniqId = $_SESSION['invite_id'];
 $fileId = $_SESSION['file_id'];
-$fileName = pathinfo($_SERVER['PATH_INFO'], PATHINFO_FILENAME);
+$fileName = pathinfo($_SERVER['REQUEST_URI'], PATHINFO_FILENAME);
 printf("var %s='%s';",$uniqId,$fileId===$fileName?fa16e3ac:$fileName.base64_encode($uniqId));
