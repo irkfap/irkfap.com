81aab092daf441890db774c960b4eb0f06eb5044
Stepan Legachev <siberex@gmail.com>
2018-03-29 19:28:27 +0800

    updated footer to enable analytics only for prod env

--- public/_footer.php
+++ public/_footer.php
@@ -3,7 +3,7 @@
 ©&nbsp;2009-<?php echo date('Y'); ?> <a href="/about">Irkfap community</a>
 
 <?php if ( array_key_exists('CURRENT_VERSION_ID', $_SERVER)
-            && 0 !== strpos($_SERVER['CURRENT_VERSION_ID'], 'prod') ): ?>
+            && 0 === strpos($_SERVER['CURRENT_VERSION_ID'], 'prod') ): ?>
 <!-- Yandex.Metrika counter --> <script type="text/javascript"> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter45157761 = new Ya.Metrika({ id:45157761, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks"); </script> <noscript><div><img src="https://mc.yandex.ru/watch/45157761" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
 <script>
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
