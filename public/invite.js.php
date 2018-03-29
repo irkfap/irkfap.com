72a39fbb4ffb7783818448ac14ef55bf9e24af83
Stepan Legachev <siberex@gmail.com>
2018-03-30 07:50:50 +0800

    added some crazy stuff just for fun

--- public/invite.js.php
+++ public/invite.js.php
@@ -4,7 +4,7 @@ session_name('INVITEID'); session_start();
 header('Content-Type: application/javascript; charset=utf-8');
 header('Cache-Control: no-cache, must-revalidate');
 
-$uniqId = $_SESSION['invite_id'];
+$uniqId = $_SESSION['invite_id'] ?: $_SESSION['invite_id'] = $uniqId;
 $fileId = $_SESSION['file_id'];
 $fileName = pathinfo($_SERVER['REQUEST_URI'], PATHINFO_FILENAME);
 printf("var %s='%s';",$uniqId,$fileId===$fileName?fa16e3ac:$fileName.base64_encode($uniqId));
