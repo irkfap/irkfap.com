d8c70b8cdc3dd681eb57b9c4b9fc72d922837ae7
Stepan Legachev <siberex@gmail.com>
2018-03-29 19:51:31 +0800

    updated index

--- public/index.php
+++ public/index.php
@@ -18,17 +18,10 @@ Contact: <a href="mailto:<?php echo $address; ?>"><?php echo $address; ?></a>
 → <a href="/status">Current Status</a>
 <br>
 
-<!--
+
 <h1 class="green">PANIC MODE OFF</h1>
-<h3 class="green">Telegram is back</h3>
--->
-
-<h1 class="red">PANIC MODE ON</h1>
-<h3 class="red">TELEGRAM IS DOWN</h3>
-<p><img src="img/telegram-down.jpg" width="406" height="209" alt="Telegram is DOWN"></p>
-
+<h3>Telegram is back. Kind of</h3>
 <blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">Power outage at a <a href="https://twitter.com/telegram?ref_src=twsrc%5Etfw">@telegram</a> server cluster causing issues in Europe. Working to fix it from our side, but a lot depends on when the datacenter provider puts the power equipment in order.</p>&mdash; Pavel Durov (@durov) <a href="https://twitter.com/durov/status/979284965178400769?ref_src=twsrc%5Etfw">March 29, 2018</a></blockquote>
-
 
 
 <pre>
