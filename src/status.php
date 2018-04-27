2d6de757ad8c3eefd84be251794e7bcc840f4298
Stepan Legachev <siberex@gmail.com>
2018-04-27 22:14:26 +0300

    fixed date

--- src/status.php
+++ src/status.php
@@ -107,9 +107,9 @@
     </dd>
 
 
-    <dt>17.04.2009</dt>
+    <dt>26.04.2009</dt>
     <dd>
-        <h1><a href="https://geektimes.ru/post/57508/" rel="nofollow">Irkfap 0.1 beta launched</a>!</h1>
+        <h1><a href="https://geektimes.com/post/57508/" rel="nofollow">Irkfap 1.0 launched</a>!</h1>
     </dd>
 
     <dt></dt>
