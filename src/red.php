55a71dba8991cb92980f351027d0d0b22534d3ea
Stepan Legachev <siberex@gmail.com>
2018-04-30 16:39:07 +0300

    added .RED announcement page

---
+++ src/red.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <?php include_once './_header.php'; ?>
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:700" rel="stylesheet">
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        h1 {
+            font-family: 'Source Sans Pro', sans-serif;
+            font-size: 70pt;
+            color: #F00;
+            text-align: center;
+        }
+    </style>
+</head>
+<body role="document">
+
+<h1>COMING SOON</h1>
+
+<?php require_once './_ga.php'; ?>
+</body>
+</html>
