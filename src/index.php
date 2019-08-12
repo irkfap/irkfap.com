5ed6ffbd47184ff2225867f36d68c84d1e07234d
Stepan Legachev <gitlog@sib.li>
2019-08-12 15:36:54 +0800

    feat(index): remove old stuff

--- src/index.php
+++ src/index.php
@@ -40,51 +40,6 @@ Contact: <a href="mailto:<?php echo $address; ?>"><?php echo $address; ?></a>
 </pre>
 <br><br>
 
-<!--<p>How deep you should regret about not selling Bitcoin half a year ago? Answer is right here!</p>
--->
-<!--
-<div class="btcwdgt-chart" bw-theme="light"></div>
-<script>
-    (function(b,i,t,C,O,I,N) {
-        window.addEventListener('load',function() {
-            if(b.getElementById(C))return;
-            I=b.createElement(i),N=b.getElementsByTagName(i)[0];
-            I.src=t;I.id=C;N.parentNode.insertBefore(I, N);
-        },false)
-    })(document,'script','https://widgets.bitcoin.com/widget.js','btcwdgt');
-</script>
--->
-
-<!-- TradingView Widget BEGIN
-<div style="max-width: 600px; margin: 20px 0;">
-    <div id="tv-medium-widget-a789d"></div>
-</div>
-<script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
-<script type="text/javascript">
-    new TradingView.MediumWidget({
-        "container_id": "tv-medium-widget-a789d",
-        "symbols": [
-            [
-                "ETH",
-                "COINBASE:ETHUSD|3m"
-            ],
-            [
-                "BTC",
-                "COINBASE:BTCUSD|3m"
-            ]
-        ],
-        "gridLineColor": "#e9e9ea",
-        "fontColor": "#83888D",
-        "underLineColor": "#dbeffb",
-        "trendLineColor": "#4bafe9",
-        "width": "600px",
-        "height": "330px",
-        "locale": "en",
-        "chartOnly": true
-    });
-</script>
-<!-- TradingView Widget END -->
-
 <?php include __DIR__ . '/_footer.php'; ?>
 </body>
 </html>
