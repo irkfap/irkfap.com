5049f54bfc29f89f9584d468f583bd1cc4aa0422
Stepan Legachev <gitlog@sib.li>
2020-11-15 04:09:52 +0300

    perf: add script to compare response times from different App Engine regions

---
+++ scripts/curl-time.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# For a set of provided URLs, measure average response time for each one.
+# https://gist.github.com/siberex/9adb939b1c7872404672697ee7985188
+# curl https://gist.githubusercontent.com/siberex/9adb939b1c7872404672697ee7985188/raw/9fc0f3fbd615e527c103ef892745a6918cefa2ed/curl_time_request.sh | bash
+
+NUM_REQUESTS=100
+
+URLS=()
+while IFS='' read -r line; do URLS+=("$line"); done < <(cat << END_OS_LIST
+be.ololo.li
+ch.ololo.li
+de.ololo.li
+END_OS_LIST
+)
+
+for url in ${URLS[*]}; do
+  url=$(echo | awk -v url="$url" 'BEGIN {OFS=""} {if (url ~ /^https?:\/\//) print url; else print "https://", url}')
+  #echo "Measuring response time for $url"
+
+  req_time_sum=0
+  for (( i=1; i<=NUM_REQUESTS; i++ )); do
+    # Measure either %{time_starttransfer} or %{time_total}
+    req_time=$(curl -s -o /dev/null -w "%{time_total}" "$url")
+    #echo "$req_time"
+    req_time_sum=$(awk "BEGIN{ print $req_time_sum + $req_time }")
+  done
+
+  req_avg_time=$(awk "BEGIN{ print $req_time_sum / $NUM_REQUESTS }")
+
+  printf "%s\t%s\n" "$url" "$req_avg_time"
+done
