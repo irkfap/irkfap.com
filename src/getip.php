60e65ce23fa1eae44a342c4f8a9e3195fa40b55b
Stepan Legachev <gitlog@sib.li>
2020-11-15 08:56:40 +0300

    fix: /getip behind CloudFlare should work now

--- src/getip.php
+++ src/getip.php
@@ -1,2 +1,15 @@
 <?php
-echo $_SERVER['REMOTE_ADDR'];
+echo getIp();
+
+function getIp() {
+    if (array_key_exists('HTTP_CF_CONNECTING_IP', $_SERVER)) {
+        return $_SERVER['HTTP_CF_CONNECTING_IP'];
+    }
+    if (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER)) {
+        return trim(explode(',', $_SERVER['HTTP_X_FORWARDED_FOR'])[0]);
+    }
+    if (array_key_exists('HTTP_X_APPENGINE_USER_IP', $_SERVER)) {
+        return $_SERVER['HTTP_X_APPENGINE_USER_IP'];
+    }
+    return $_SERVER['REMOTE_ADDR'];
+}
